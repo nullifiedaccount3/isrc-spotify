a379cfce6bf3a424685e2eb3c180bcdb914a489a
Bhargav Nanekalva <bhargav@divum.in>
2018-08-02 08:55:27 +0530

    missing entries fix

--- app/Jobs/ISRCExporter.php
+++ app/Jobs/ISRCExporter.php
@@ -51,61 +51,65 @@ class ISRCExporter implements ShouldQueue
         $this->api = new SpotifyWebAPI();
         $this->user = User::find($this->inputs['user_id']);
 
-        $this->refresh_token();
-
         //Export ISRC data to file
         $this->api->setAccessToken($this->user->spotify_token);
         $limit = 50;
         $offset = 0;
         $this->index = 1;
 
-        Storage::disk('local')->put($this->inputs['query'] . '.tsv', null);
-        $this->tsv = implode("\t", ['Index',
+        $tsv_header = implode("\t", [
+            'Index',
             'Track Album Art',
             'Artist name',
             'Album Name',
             'Track Name',
             'ISRC',
-            'Track Duration']);
+            'Track Duration'
+        ]);
+
+        Storage::disk('local')->put($this->inputs['query'] . '.tsv', $tsv_header);
 
         $this->data = $this->api->search($this->inputs['query'], 'track', [
             'limit' => $limit,
             'offset' => $offset
         ]);
+        $this->fetch_isrc();
 
         while (!empty($this->data->tracks->items)) {
-            $this->fetch_isrc();
-            $offset = $offset + $limit + 1;
+            $offset = $offset + $limit;
+            $this->refresh_token();
             $this->data = $this->api->search($this->inputs['query'], 'track', [
                 'limit' => $limit,
                 'offset' => $offset
             ]);
+            $this->fetch_isrc();
         }
     }
 
     private function fetch_isrc()
     {
-        $this->refresh_token();
+        $tsv = null;
         foreach ($this->data->tracks->items as $track) {
-            $this->tsv = $this->tsv . PHP_EOL;
             $artists = collect($track->album->artists)->map(function ($artist) {
-                return $artist->name;
+                return isset($artist->name) ? $artist->name : '';
             })->toArray();
             $artists = implode(", ", $artists);
             try {
-                $this->tsv = $this->tsv . implode("\t", [$this->index,
-                        $track->album->images[0]->url,
-                        $artists,
-                        $track->album->name,
-                        $track->name,
-                        $track->external_ids->isrc,
-                        round($track->duration_ms / 60000, 2)]);
+                $tsv = implode("\t", [
+                    $this->index,
+                    isset($track->album->images[0]->url) ? $track->album->images[0]->url : '',
+                    $artists,
+                    isset($track->album->name) ? $track->album->name : '',
+                    isset($track->name) ? $track->name : '',
+                    isset($track->external_ids->isrc) ? $track->external_ids->isrc : '',
+                    isset($track->duration_ms) ? round($track->duration_ms / 60000, 2) : ''
+                ]);
                 $this->index = $this->index + 1;
+                Storage::disk('local')->append($this->inputs['query'] . '.tsv', $tsv);
             } catch (\Exception $exception) {
                 Log::error(json_encode($track));
             }
         }
-        Storage::disk('local')->append($this->inputs['query'] . '.tsv', $this->tsv);
     }
 
     private function refresh_token()
